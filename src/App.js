45eeb8c4445971b50be5d2d4b972ef6883495aa6
Eugénie <eugenie.philippe@hotmail.fr>
2022-12-05 17:43:12 +0100

    Initialization - modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import FormModal from './lib/formModal'
+import { useState, useEffect} from 'react'
 
 function App() {
+
+  const [openModal, setOpenModal] = useState(false);
+  useEffect(() => {
+    setOpenModal(true);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Modal">
+      {openModal && (
+        <FormModal closeModal={() => setOpenModal(false)}/>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/lib/formModal.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const FormModal = ( props ) => {
+    const { closeModal } = props
+    
+    return (
+      <div className='modalBackground'>
+        <div className="container">
+          <div className="icon-text">
+              <div className="icon-container" onClick={closeModal} aria-label="cross-icon">
+                  <i className="fa fa-lg fa-times" aria-hidden="true"></i>
+              </div>
+              <p>Employee successfully created !</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  export default FormModal
